3a9f933fb3ced0fa745f484773d89a5f33bbe8ee
Joaogdf <joaogustavone@gmail.com>
2026-03-10 15:44:50 +0000

    add css styles, register user form and api integration

---
+++ frontend/src/components/forms/register-form.tsx
@@ -0,0 +1,93 @@
+"use client";
+import { useActionState, useEffect } from "react";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import { registerAction } from "@/actions/auth";
+import { useRouter } from "next/navigation";
+
+export function RegisterForm() {
+  const [state, formAction, isPending] = useActionState(registerAction, null);
+  const router = useRouter();
+  useEffect(() => {
+    if (state?.success && state?.redirectTo) {
+      router.replace(state.redirectTo);
+    }
+  }, [state, router]);
+  return (
+    <Card className="bg-app-card border-app-border w-full max-w-md mx-auto">
+      <CardHeader>
+        <CardTitle className="text-white text-center">
+          <img src="/logo.png" alt="Logo" className="rounded-md w-24 mx-auto" />
+        </CardTitle>
+        <CardDescription className="text-center text-xs">
+          Create an account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form className="space-y-4" action={formAction}>
+          <div className="space-y-2">
+            <Label htmlFor="name" className="text-white">
+              Name
+            </Label>
+            <Input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Insert your name"
+              required
+              minLength={3}
+              className="text-white bg-app-card border-app-border"
+            ></Input>
+          </div>
+          <div className="space-y-2">
+            <Label htmlFor="email" className="text-white">
+              Email
+            </Label>
+            <Input
+              type="text"
+              id="email"
+              name="email"
+              placeholder="Insert your email address"
+              required
+              className="text-white bg-app-card border-app-border"
+            ></Input>
+          </div>
+          <div className="space-y-2">
+            <Label htmlFor="password" className="text-white">
+              Password
+            </Label>
+            <Input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Insert your password"
+              required
+              className="text-white bg-app-card border-app-border"
+            ></Input>
+          </div>
+          <Button
+            type="submit"
+            className="w-full text-white bg-brand-primary hover:bg-brand-primary font-semibold"
+          >
+            {isPending ? "Creating account..." : "Create account"}
+          </Button>
+          <p className="text-center text-gray-100">
+            Already have an account?{" "}
+            <Link href="/login" className="text-brand-primary font-semibold">
+              Log in
+            </Link>
+          </p>
+        </form>
+      </CardContent>
+    </Card>
+  );
+}
