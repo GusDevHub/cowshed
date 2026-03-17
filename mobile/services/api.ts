001dc2e425bd2674f54fc9f8750c40e67b138628
Joaogdf <joaogustavone@gmail.com>
2026-03-17 11:00:32 +0000

    feat: add axios and create api in mobile

---
+++ mobile/services/api.ts
@@ -0,0 +1,35 @@
+import { API_CONFIG } from "@/config/api.config";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+const api = axios.create({
+  baseURL: API_CONFIG.BASE_URL,
+  timeout: API_CONFIG.TIMEOUT,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem("@restaurant:token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response?.status === 401) {
+      await AsyncStorage.removeItem("@restaurant:token");
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default api;
